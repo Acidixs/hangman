c366b39c07b77f1bf15b3c4c5bea6789216c80b1
Acidixs <65794019+Acidixs@users.noreply.github.com>
2020-09-03 21:32:51 +0200

    fixed stuff

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import time
+import string
 
 from hangman_pics import HANGMANPICS
 
 word = "Python".lower()
 blanks = list("_" * len(word))
+alphabet = list(string.ascii_lowercase)
 
 guessed_letters = []
 wrong_letters = []
@@ -11,31 +13,38 @@ attempts = 7
 
 while attempts > 0:
 
-    if "".join(guessed_letters) == word:
-        print("You win!")
-        exit(1)
+        if "".join(guessed_letters) == word:
+            print("You win!")
+            exit(1)
 
-    print("HANGMAN")
-    print(HANGMANPICS[len(wrong_letters)])
+        print("HANGMAN")
+        print(HANGMANPICS[len(wrong_letters)])
 
-    print("".join(blanks))
-    guess = str(input("Guess a letter: ")).lower()
-
-    if guess in guessed_letters:
-        print("You have already guessed that letter")
-        time.sleep(2)
-        continue
-
-    guessed_letters.append(guess)
-
-    if guess in word:
-        wordIndex = word.find(guess)
-        blanks[wordIndex] = guess
         print("".join(blanks))
+        guess = str(input("Guess a letter: ")).lower()
 
-    else:
-        wrong_letters.append(word)
-        attempts -= 1
+
+        if guess not in alphabet or len(guess) >= 2:
+            print("Enter a single letter")
+            time.sleep(2)
+            continue
+
+        if guess in guessed_letters:
+            print("You have already guessed that letter")
+            time.sleep(2)
+            continue
+
+        guessed_letters.append(guess)
+
+
+        if guess in word:
+            wordIndex = word.find(guess)
+            blanks[wordIndex] = guess
+            print("".join(blanks))
+
+        else:
+            wrong_letters.append(word)
+            attempts -= 1
 
 print(f"You are out of attempts! The word was {word}")
-exit(1)
+#playAgain = input("Do you want to play again? Y/N")
